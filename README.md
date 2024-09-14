90c8c9be5f07143a7dac82aa483563a5aa09df9f
AtharvaGandhi <gamagod124@gmail.com>
2024-09-14 21:25:06 +0530

    tts

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # AI-BOT-AVA
+
 This repo is official repo for BE Major project
 AVA would be a voice assitive robot for differently abled people.
 
-Download: https://ollama.com/download/windows
-cmd (admin) : ollama run llama3
-Terminal: pip install ollama
+- Download: https://ollama.com/download/windows
+- cmd (admin) : ollama run llama3
+- Terminal: pip install ollama
 
-create environment: python -m venv venv
-go to enviornment: .\venv\Scripts\activate
+- create environment: python -m venv venv
+- activate enviornment: .\venv\Scripts\activate
--- api_testing.py
+++ api_testing.py
@@ -1,10 +1,27 @@
 import ollama
+import pyttsx3
+
+# Initialize the pyttsx3 TTS engine
+engine = pyttsx3.init()
+
+
+# Function to convert text to speech
+def speak_text(text):
+    engine.say(text)
+    engine.runAndWait()
+
 
 # Example of a chat session
 response = ollama.chat(
-    model="llama3",  # Or 'llama3' if available in your setup
-    messages=[{"role": "user", "content": 'no of words in "hello world as "'}],
+    model="llama3",  # Or 'llama3' if available
+    messages=[{"role": "user", "content": "Why is the sky blue?"}],
 )
 
+# Extract the chatbot's response
+answer = response["message"]["content"]
+
 # Print the response
-print(response["message"]["content"])
+print(answer)
+
+# Convert the response to speech
+speak_text(answer)
