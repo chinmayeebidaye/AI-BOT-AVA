fb3d85bbc205e3380e1952c140b6aef63e185384
Swayam Dhanawade <swayamd1203@gmail.com>
2024-09-17 13:07:30 +0530

    Recent Changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=X:\1 Final year project\AI-BOT-AVA\venv
+set VIRTUAL_ENV=C:\Users\Swayam\Desktop\AI-BOT-AVA\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
